43cc0161cd4fdd2fa1cbbd71c0e13046450e0fde
Andreas Radauer <andreas.radauer@porscheinformatik.at>
2017-05-08 15:58:32 +0200

    added real questions

--- src/main/java/com/radauer/Questions.java
+++ src/main/java/com/radauer/Questions.java
@@ -13,19 +13,24 @@ public class Questions
     public Questions()
     {
         this.questions = new ArrayList<>();
-        for (int i = 0; i < 3; i++)
-        {
-            Question question = new Question();
-            question.setQuestion("This is Question # " + (i + 1));
-            question.setAnswers(new String[4]);
-            question.setCorrectAnswer(0);
-            questions.add(question);
 
-            for (char c = 'A'; c < 'E'; c++)
-            {
-                question.getAnswers()[c - 'A'] = "answer " + c;
-            }
-        }
+        Question question = new Question();
+        question.setQuestion("How many digitisation experts are working for Porsche Informatik?");
+        question.setAnswers(new String[]{"42", "450", "0111001"});
+        question.setCorrectAnswer(1);
+        questions.add(question);
+
+        question.setQuestion("How many filter criteria do you see on the first page of https://www.dasweltauto.at?");
+        question.setAnswers(new String[]{"50", "3", "10"});
+        question.setCorrectAnswer(2);
+        questions.add(question);
+
+        question.setQuestion(
+            "In which field of our Digital Innovation department is the virtual testdrive application situated?");
+        question.setAnswers(new String[]{"Virtual Reality", "Artificial Intelligence", "Big Data"});
+        question.setCorrectAnswer(0);
+        questions.add(question);
+
     }
 
     public Question getQuestion(int currentQuestion)
